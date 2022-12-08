3b79708297bf8314b463e3110fc03f005e47eca5
Bryn Ryans <bnryans@google.com>
2022-12-08 10:15:54 -0800

    feat: propagate abort signal to message receiver
    The initial AbortSignal implementation only cancelled
    the chatty message sender. This change propagates abort
    signals to the chatty message receiver. The caller MUST
    opt in to propagate the signal as the receiving function
    needs to be aware that the signal will be passed as the
    last parameter of function arguments.
    
    Note this version is compatible with other versions of
    chatty.

--- src/types.ts
+++ src/types.ts
@@ -39,6 +39,14 @@ export interface CallbackStore {
 }
 
 /**
+ * A container for [[AbortController]] instances indexed by event name. and sequence
+ */
+
+export interface AbortControllerStore {
+  [nameSequence: string]: AbortController
+}
+
+/**
  * Options associated with a sendReceive request
  */
 
@@ -49,6 +57,12 @@ export interface Options {
    */
 
   signal: AbortSignal
+
+  /**
+   * Propogate signal used to propagate a signal abort to the message
+   * receiver.
+   */
+  propagateSignal?: boolean
 }
 
 /**
